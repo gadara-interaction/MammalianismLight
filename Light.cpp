c20cac2a97fec4f7105ed4fc0bc76a2db21b8c2a
minatcher <minatcher@users.noreply.github.com>
2019-05-04 23:51:52 +0900

    Add files via upload
    add LED 3 to 5

--- Light.cpp
+++ Light.cpp
@@ -2,7 +2,7 @@
 #include <Adafruit_NeoPixel.h>
 
 //LED 3つ, D2に接続
-Adafruit_NeoPixel pixel = Adafruit_NeoPixel(3, 2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(5, 2, NEO_GRB + NEO_KHZ800);
 
 //増やす場合
 //Adafruit_NeoPixel pixel = Adafruit_NeoPixel(4, 2, NEO_GRB + NEO_KHZ800);
@@ -17,7 +17,8 @@ void initLight(){
   pixel.setPixelColor(0, pixel.Color(255,168,97)); //一番最初のLEDは０番です
   pixel.setPixelColor(1, pixel.Color(255,168,97)); //一番最初のLEDは０番です
   pixel.setPixelColor(2, pixel.Color(255,168,97)); //一番最初のLEDは０番です
-//  pixel.setPixelColor(3, pixel.Color(255,168,97)); //一番最初のLEDは０番です
+  pixel.setPixelColor(3, pixel.Color(255,168,97)); //一番最初のLEDは０番です
+  pixel.setPixelColor(4, pixel.Color(255,168,97));
   pixel.setBrightness(255);
   pixel.show();
 }
@@ -25,4 +26,3 @@ void initLight(){
 void blinkLight(){
 
 }
-
